a4d18d99edec17adf464a4d008801de6663012e0
Giyanz <gruperez@ssct.edu.ph>
2026-05-03 14:15:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = 'secret123'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/agrichain_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join('static', 'uploads') 
+    UPLOAD_FOLDER = os.path.join('Static', 'uploads') 
     ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
     ADMIN_MASTER_KEY = os.environ.get('ADMIN_MASTER_KEY', 'agrichain123')
